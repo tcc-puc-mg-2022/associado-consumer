8fa001f55cb5e0ad3828475776feb77e7a908ec5
Ueverton Leomir <ueverton@gmail.com>
2022-02-05 17:27:21 -0300

    ajustes de ambiente

--- src/main/java/br/com/pucminas/associado/associadoconsumer/configuration/QueueConfig.java
+++ src/main/java/br/com/pucminas/associado/associadoconsumer/configuration/QueueConfig.java
@@ -3,6 +3,7 @@ package br.com.pucminas.associado.associadoconsumer.configuration;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
@@ -10,11 +11,20 @@ import java.util.concurrent.TimeoutException;
 @Configuration
 public class QueueConfig {
 
+    @Value("${spring.rabbitmq.host}")
+    private String rabbitmqHost;
+
+    @Value("${spring.rabbitmq.username}")
+    private String rabbitmqUser;
+
+    @Value("${spring.rabbitmq.password}")
+    private String rabbitmqPassword;
+
     public Connection getConnection() throws IOException, TimeoutException {
         final var factory = new ConnectionFactory();
-        factory.setHost("localhost");
-        factory.setUsername("admin");
-        factory.setPassword("admin");
+        factory.setHost(rabbitmqHost);
+        factory.setUsername(rabbitmqUser);
+        factory.setPassword(rabbitmqPassword);
 
         return factory.newConnection();
     }
